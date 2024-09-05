f77ea9edf3e17b75f29ffba94016270f9047ce69
Jordan Da Graca <jordan.dagraca@damteq.co.uk>
2024-09-05 11:06:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,76 @@
-// ENV
-import 'dotenv/config';
-
 // Puppeteer
 import puppeteer from 'puppeteer';
-const site = 'https://kinetecuk.com/letmein';
+// Dotenv
+import 'dotenv/config';
+// Slack Integration
+import Slack from '@slack/bolt';
+// SQL DB
+import mysql from 'mysql2';
 
-// lOCALHOST setup
-import http from "http";
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-    res.write('test');
-    res.end();
+// DB Connection
+var con = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_DB
 });
 
-server.listen(port, () => {
-    console.log(`Server is running on port ${port}.`);
+// Slack Connection
+const app = new Slack.App({
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN
 });
 
-// Launch the browser and open a new blank page
-const browser = await puppeteer.launch({
-    headless: false, // Displays actions in viewport window
-    defaultViewport: false, // Overrides default viewport to display window full size.
-    userDataDir: './tmp',
+// DB Query
+con.connect(function(err) {
+    if (err) throw err;
+    con.query("SELECT Client_Name, Client_Url FROM damteq_clients", async function (err, results, fields) {
+        if (err) throw err;
+        const entries = results.map(row => row.Client_Url);
+
+        // Commented out 10-min interval to mimic running a cron on the file.
+        // let interval = 10 * 60 * 1000;
+        // setInterval(async () => {
+        await runPuppeteer(entries);
+        // }, interval);
+    });
 });
-const [page] = await browser.pages();
-// Navigate the page to a URL.
-await page.goto(site, {
-    waitUntil: 'networkidle2',
-});
-// PAGE SPECIFIC STUFF //
-// Login to Kinetec //
-await page.locator('#user_login').fill('damteq');
-await page.locator('#user_pass').fill(process.env.KINETECPASS);
-await page.locator('#wp-submit').click();
-await page.waitForNavigation();
-// Listen for all responses
-page.on('response', async (response) => {
-    // Check if the status code is 500
-    if (response.status() === 500) {
-        // Unsuccessfully loads site
-        console.error(`500 error detected: ${response.url()}`);
-        await page.close();
-    } else if (response.status() === 200) {
-        // Successfully loads site
-        console.log('Site loads as expected');
-        await page.close();
+
+//DB INSERT INTO
+// con.connect(function(err) {
+//     if (err) throw err;
+//     var sql = "INSERT INTO damteq_clients (Client_Name, Client_Url) VALUES ?";
+//     var values = [
+//       ['Marine Lighting Store', 'https://www.marinelightingstore.com']
+//     ];
+//     con.query(sql, [values], function (err, result) {
+//       if (err) throw err;
+//       console.log("Number of records inserted: " + result.affectedRows);
+//     });
+//   });
+
+// Function to run Puppeteer loop
+async function runPuppeteer(entries) {
+    const browser = await puppeteer.launch();
+    for (let url of entries) {
+        const page = await browser.newPage();
+        try {
+            const response = await page.goto(url, { waitUntil: 'load', timeout: 0 });
+            const status = response.status();
+            if (status >= 400) {
+                await app.client.chat.postMessage({
+                    token: process.env.SLACK_BOT_TOKEN,
+                    channel: process.env.SLACK_BOT_CHANNEL,
+                    text: `:x: Outage Detected: ${url} has encountered a ${status} error.`, // Add <!channel> to mention channel
+                });
+            }
+        } catch (error) {
+            console.error(`Error: Could not navigate to ${url} - ${error.message}`);
+            await browser.close();
+            process.exit(1);
+        } finally {
+            await page.close();
+        }
     }
-});
+    await browser.close();
+}
